3c24267685dcddda6b09e544ca1fba35b2e1dd19
Kom Bacho <bachokom5@gmail.com-github@gmail.com>
2026-01-24 02:11:10 +0100

    push 03

--- accueil/urls.py
+++ accueil/urls.py
@@ -3,16 +3,12 @@ from django.conf.urls.static import static
 from . import views
 from django.urls import path 
 
+
 app_name = 'accueil'
 
 urlpatterns = [
-    # La page d'accueil (votre interface orange et blanche)
-    path('', views.accueil, name='accueil'),
-    
-    # La route API pour le bouton "Localiser"
-    path('api/recherche/', views.rechercher_artistes, name='api-recherche'),
-]
+    path('', views.accueil, name='accueil')
 
-# Gestion des médias (indispensable pour les portfolios d'artistes)
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
